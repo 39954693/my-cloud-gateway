bb1f3062754162d7202733f05cc54c3e25888213
dinghaocheng <dinghaocheng@sunline.cn>
2020-12-18 13:06:33 +0800

    复制请求头和添加过滤器

--- src/main/java/tech/liqun/cloud/gateway/GatewayController.java
+++ src/main/java/tech/liqun/cloud/gateway/GatewayController.java
@@ -1,8 +1,5 @@
 package tech.liqun.cloud.gateway;
 
-import org.springframework.core.io.buffer.DataBufferFactory;
-import org.springframework.core.io.buffer.DefaultDataBufferFactory;
-import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,29 +7,24 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
+import java.util.Objects;
 
-/**
- * @Author dinghaocheng
- * @Date 2020/12/17 10:53 上午
- * @Version 1.0
- **/
 @RestController
 public class GatewayController {
     private final WebClient webClient;
-    private final DataBufferFactory bufferFactory;
 
-    public GatewayController() {
-        webClient = WebClient.builder().build();
-        bufferFactory = new DefaultDataBufferFactory();
+    public GatewayController(WebClient webClient) {
+        this.webClient = webClient;
     }
 
+    //TODO: plugin to request mappings
     @GetMapping("/")
     public Mono<Void> home(ServerWebExchange exchange) {
-        return this.webClient.get().uri(URI.create("http://httpbin.org/get")).accept(MediaType.APPLICATION_JSON)
+        return this.webClient.get().uri((String) Objects.requireNonNull(exchange.getAttribute("requestUrl")))
+                .headers(httpHeaders -> httpHeaders.addAll(exchange.getRequest().getHeaders()))
                 .exchange().flatMap(clientResponse -> {
                     ServerHttpResponse response = exchange.getResponse();
-                    response.getHeaders().setContentType(clientResponse.headers().contentType().get());
+                    response.getHeaders().putAll(clientResponse.headers().asHttpHeaders());
                     response.setStatusCode(clientResponse.statusCode());
                     return response.writeWith(clientResponse.body((inputMessage, context) -> inputMessage.getBody()));
                 });
--- src/main/java/tech/liqun/cloud/gateway/MyCloudGatewayApplication.java
+++ src/main/java/tech/liqun/cloud/gateway/MyCloudGatewayApplication.java
@@ -2,10 +2,35 @@ package tech.liqun.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.WebFilter;
 
 @SpringBootApplication
 public class MyCloudGatewayApplication {
 
+	@Bean
+	public WebClient webClient(){
+		return WebClient.create();
+	}
+	@Bean
+	@Order(500)
+	public WebFilter findRouteFilter(){
+		return (exchange, chain) -> {
+			exchange.getAttributes().put("requestUrl", "http://httpbin.org/get");
+			return chain.filter(exchange);
+		};
+	}
+	// TODO: request only, how to filter response?
+	@Bean
+	@Order(501)
+	public WebFilter modifyResponseFilter() {
+		return (exchange, chain) -> {
+			exchange.getResponse().getHeaders().add("X-My-Custom", "MyCustomValue");
+			return chain.filter(exchange);
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MyCloudGatewayApplication.class, args);
 	}
